d04f4ceeb7086d171ff6241d7518f0d16fb96554
amc <cibich.a@gmail.com>
2021-10-30 16:07:31 -0500

    test page creation

---
+++ src/pages/about.js
@@ -0,0 +1,31 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import { Link } from "gatsby"
+import Seo from "../components/seo"
+import { StaticImage } from "gatsby-plugin-image"
+
+const AboutPage = () => ( //is a function that calls a jsx
+  <Layout>
+    <Seo title="About" />
+    <h1>Details</h1>
+    <p>Welcome to my portfolio.</p>
+    <p>Check out more about me.</p>
+    <StaticImage
+      src="../images/gatsby-astronaut.png"
+      width={300}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <p>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
+      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
+      <Link to="/using-dsg">Go to "Using DSG"</Link>
+    </p>
+  </Layout>
+)
+
+export default AboutPage
